7aef939a3c2a2eac68b6704d2107375ea5821a76
Amy Murphy <amurphy@spartaglobal.com>
2021-09-23 12:01:34 +0100

    updated alarms in main

--- main.tf
+++ main.tf
@@ -253,27 +253,51 @@ resource "aws_autoscaling_policy" "sre_amy_terraform_as_policy" {
         predefined_metric_specification {
             predefined_metric_type = "ASGAverageCPUUtilization"
         }
-        target_value = 3.0
+        target_value = 2.0
     }
 }
 
-resource "aws_autoscaling_policy" "sre_amy_scale_down_policy" {
-  name = "sre_amy_scale_down_policy"
-  scaling_adjustment = -1
-  adjustment_type = "ChangeInCapacity"
-  cooldown = 300
-  autoscaling_group_name = aws_autoscaling_group.sre_amy_terraform_autoscaling_group.name
+resource "aws_autoscaling_policy" "sre_amy_CPU_scale_down_policy" {
+    name = "sre_amy_CPU_scale_down_policy"
+    scaling_adjustment = -1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = aws_autoscaling_group.sre_amy_terraform_autoscaling_group.name
 }
 
-resource "aws_cloudwatch_metric_alarm" "sre_amy_scale_down_alarm" {
-  alarm_name = "sre_amy_scale_down_alarm"
-  comparison_operator = "LessThanThreshold"
-  metric_name = "NetworkIn"
-  statistic = "Average"
-  threshold = "500000"
-  period = "120"
-  evaluation_periods = "2"
-  namespace = "AWS/EC2"
-  alarm_description = "Monitors ASG EC2 average network in (for scale down policy)"
-  alarm_actions = [aws_autoscaling_policy.sre_amy_scale_down_policy.arn]
+resource "aws_cloudwatch_metric_alarm" "sre_amy_CPU_scale_down_alarm" {
+    alarm_name = "sre_amy_CPU_scale_down_policy"
+    comparison_operator = "LessThanThreshold"
+
+    metric_name = "CPUUtilization"
+    statistic = "Average"
+
+    threshold = 2
+    period = 120
+    evaluation_periods = 2
+
+    namespace = "AWS/EC2"
+    alarm_description = "Monitors ASG EC2 average cpu utilization (for scale down policy)"
+    alarm_actions = [aws_autoscaling_policy.sre_amy_CPU_scale_down_policy.arn]
 }
+
+# resource "aws_autoscaling_policy" "sre_amy_scale_down_policy" {
+#   name = "sre_amy_scale_down_policy"
+#   scaling_adjustment = -1
+#   adjustment_type = "ChangeInCapacity"
+#   cooldown = 300
+#   autoscaling_group_name = aws_autoscaling_group.sre_amy_terraform_autoscaling_group.name
+# }
+
+# resource "aws_cloudwatch_metric_alarm" "sre_amy_scale_down_alarm" {
+#   alarm_name = "sre_amy_scale_down_alarm"
+#   comparison_operator = "LessThanThreshold"
+#   metric_name = "NetworkIn"
+#   statistic = "Average"
+#   threshold = "500000"
+#   period = "120"
+#   evaluation_periods = "2"
+#   namespace = "AWS/EC2"
+#   alarm_description = "Monitors ASG EC2 average network in (for scale down policy)"
+#   alarm_actions = [aws_autoscaling_policy.sre_amy_scale_down_policy.arn]
+# }
